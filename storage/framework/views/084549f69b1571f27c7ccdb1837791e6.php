92e8bb75f28659595aca54dfb416208c56106d4f
dsnmel <95360813+dsnmel@users.noreply.github.com>
2025-11-15 23:22:14 +0700

    Add files via upload

---
+++ storage/framework/views/084549f69b1571f27c7ccdb1837791e6.php
@@ -0,0 +1,25 @@
+
+
+<?php $__env->startSection('content'); ?>
+<h1>Tambah Produk</h1>
+
+<form method="POST" action="<?php echo e(route('products.store')); ?>">
+    <?php echo csrf_field(); ?>
+
+    <label>Nama Produk</label>
+    <input type="text" name="name" required>
+
+    <label>Deskripsi</label>
+    <textarea name="description"></textarea>
+
+    <label>Harga</label>
+    <input type="number" step="0.01" name="price" required>
+
+    <label>Stok</label>
+    <input type="number" name="stock" value="0" required>
+
+    <button class="submit-btn">Simpan</button>
+</form>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\lenov\crud-product-app\resources\views/products/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4d4ba9ca20a7e9081157b29e03a9d356.php
@@ -0,0 +1,26 @@
+
+
+<?php $__env->startSection('content'); ?>
+<h1>Edit Produk</h1>
+
+<form method="POST" action="<?php echo e(route('products.update', $product->id)); ?>">
+    <?php echo csrf_field(); ?>
+    <?php echo method_field('PUT'); ?>
+
+    <label>Nama Produk</label>
+    <input type="text" name="name" value="<?php echo e($product->name); ?>" required>
+
+    <label>Deskripsi</label>
+    <textarea name="description"><?php echo e($product->description); ?></textarea>
+
+    <label>Harga</label>
+    <input type="number" step="0.01" name="price" value="<?php echo e($product->price); ?>" required>
+
+    <label>Stok</label>
+    <input type="number" name="stock" value="<?php echo e($product->stock); ?>" required>
+
+    <button class="submit-btn">Update</button>
+</form>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\lenov\crud-product-app\resources\views/products/edit.blade.php ENDPATH**/ ?>
